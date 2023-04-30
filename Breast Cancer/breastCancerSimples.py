9e848ba641594ae2b71a3b61dbd707203a822fc5
Marcus Castilho <61066188+castpyGH@users.noreply.github.com>
2023-04-30 13:03:35 -0300

    Configuração inicial da rede neural
    Essa configuração inicial da rede neural indica a importação dos módulos, a criação dos neurônios de entrada, a primeira camada oculta ativada com a função RELU e a camada de saída com a função de ativação SIGMOID.

---
+++ Breast Cancer/breastCancerSimples.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 17:06:41 2023
+
+@author: Castpy
+"""
+
+import pandas as pd
+
+#Lendo arquivos de entrada e saída
+previsores = pd.read_csv("entradas_breast.csv")
+classe = pd.read_csv("saidas_breast.csv")
+
+#Divindo a base entre treinamento e teste
+from sklearn.model_selection import train_test_split
+#Essas variaveis recebem a divisão das bases feita automaticamente pelo modulo importado a cima.
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
+
+#Implementando a estrutura da rede neural
+import keras
+from keras.models import Sequential
+#O módulo DENSE é para interligar cada neuronio com todos os subsequentes a sua frente
+from keras.layers import Dense
+
+#Criando a rede
+classificador = Sequential()
+#Criando a primeira camada oculta
+
+#units = QUANTIDADE DE NEURONIOS NA CAMADA OCULTA
+#para saber a quantidade correta, use a quantidade de entradas + quantidade de saida / 2
+#No nosso caso, temos 30 entradas, 1 saida binária, e dividimos por 2. Terminal -> (30 + 2) / 2 = 15.5
+#O resultado da soma acima é 15.5, então podemos usar 16
+
+#activation = função de ativação usada na rede neural
+#input_dim = quantidade de entradas
+classificador.add(Dense(units = 16, activation= 'relu', kernel_initializer= 'random_uniform', input_dim= 30))
+
+
+#Camada de saída
+classificador.add(Dense(units= 1, activation='sigmoid'))
